5d343ad22fd916cfeed61d1b491c00a31e80a6fe
Uraneptus <uraneptus33@gmail.com>
2024-06-24 03:36:05 +0200

    rgrg

--- src/main/java/com/uraneptus/frycooks_delight/FrycooksDelight.java
+++ src/main/java/com/uraneptus/frycooks_delight/FrycooksDelight.java
@@ -60,14 +60,11 @@ public class FrycooksDelight {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        /*
         event.enqueueWork(() -> FluidInteractionRegistry.addInteraction(FCDFluids.HOT_GREASE_FLUID_TYPE.get(),
                 new FluidInteractionRegistry.InteractionInformation(
                 ForgeMod.WATER_TYPE.get(),
                 fluidState -> FCDBlocks.LARD_BLOCK.get().defaultBlockState()
         )));
-
-         */
     }
 
     @SubscribeEvent
